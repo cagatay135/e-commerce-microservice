2c89c9c0553eeeb945784bbd36537ca3f7e0b5fb
cagatay135 <cagatay135@gmail.com>
2024-07-22 15:47:54 +0300

    Import FeignAutoConfiguration using @ImportAutoConfiguration

--- order-service/src/main/java/com/cagatay/curuk/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/cagatay/curuk/orderservice/OrderServiceApplication.java
@@ -1,11 +1,14 @@
 package com.cagatay.curuk.orderservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
