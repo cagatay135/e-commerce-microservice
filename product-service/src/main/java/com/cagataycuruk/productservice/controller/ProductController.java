2b49cbda414a71f35009bf51ecd281f7c8e6f43a
cagatay135 <cagatay135@gmail.com>
2024-07-23 15:03:32 +0300

    Remove swagger annotations on ProductController

--- product-service/src/main/java/com/cagataycuruk/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/cagataycuruk/productservice/controller/ProductController.java
@@ -23,45 +23,18 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @Operation(summary = "Get all products")
-    @ApiResponses(value =
-    @ApiResponse(
-            responseCode = "200",
-            description = "All products",
-            content = @Content(
-                    schema = @Schema(implementation = List.class),
-                    mediaType = "application/json")))
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public List<ProductResponseDto> getAllProducts() {
         return productService.getAllProducts();
     }
 
-
-    @Operation(summary = "Get product")
-    @ApiResponses(value =
-    @ApiResponse(
-            responseCode = "200",
-            description = "Get product by productId",
-            content = @Content(
-                    schema = @Schema(implementation = ProductResponseDto.class),
-                    mediaType = "application/json")))
     @GetMapping("/{productId}")
-    @ResponseStatus(HttpStatus.OK)
     public ProductResponseDto getProductById(@PathVariable UUID productId) {
         return productService.getProductById(productId);
     }
 
-    @Operation(summary = "Create product")
-    @ApiResponses(value =
-    @ApiResponse(
-            responseCode = "201",
-            description = "Create product",
-            content = @Content(
-                    schema = @Schema(implementation = String.class),
-                    mediaType = "application/json")))
+
     @PostMapping()
-    @ResponseStatus(HttpStatus.CREATED)
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto productRequestDto) {
         return productService.createProduct(productRequestDto);
     }
