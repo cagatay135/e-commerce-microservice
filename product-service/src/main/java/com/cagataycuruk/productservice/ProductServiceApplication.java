df5a852ddc94a3ace0ad2ea1cde4a5c3c121be7f
cagatay135 <cagatay135@gmail.com>
2024-07-23 15:04:22 +0300

    Refactor product-service application.properties file

--- product-service/src/main/java/com/cagataycuruk/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/cagataycuruk/productservice/ProductServiceApplication.java
@@ -1,6 +1,5 @@
 package com.cagataycuruk.productservice;
 
-import com.cagataycuruk.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,42 +14,8 @@ public class ProductServiceApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
-    /*
-    private final OrderService orderService;
-
-    private final ProductService productService;
-    private final InventoryService inventoryService;
-     */
-
 
     @Override
     public void run(String... args) {
-        /*
-        for (int i = 0; i < 10; i++) {
-            Product product = Product.builder()
-                    .name("Product")
-                    .price(BigDecimal.valueOf(Math.random() * 1000).setScale(2, RoundingMode.FLOOR))
-                    .build();
-            productService.createProduct(product);
-
-            Inventory inventory = Inventory.builder()
-                    .product(product)
-                    .quantity((int) (Math.random() * 100))
-                    .build();
-
-            inventoryService.createInventory(inventory);
-
-            List<Product> products = new ArrayList<>();
-            products.add(product);
-
-            Order order = Order.builder()
-                    .products(products)
-                    .totalPrice(product.getPrice())
-                    .status(OrderStatus.APPROVAL_PENDING)
-                    .build();
-
-            orderService.createOrder(order);
-        }
-         */
     }
 }
