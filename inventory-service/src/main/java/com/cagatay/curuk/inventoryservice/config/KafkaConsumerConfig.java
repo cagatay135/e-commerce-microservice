f065b16c7987e814898960ecb5b49b7ce4aacf09
cagatay135 <cagatay135@gmail.com>
2024-07-30 14:20:00 +0300

    Add kafka bootstrap server to application properties

--- inventory-service/src/main/java/com/cagatay/curuk/inventoryservice/config/KafkaConsumerConfig.java
+++ inventory-service/src/main/java/com/cagatay/curuk/inventoryservice/config/KafkaConsumerConfig.java
@@ -1,2 +1,63 @@
-package com.cagatay.curuk.inventoryservice.config;public class KafkaConsumerConfig {
+package com.cagatay.curuk.inventoryservice.config;
+
+import com.cagatay.curuk.inventoryservice.dto.messages.OrderDebeziumMessage;
+import com.cagatay.curuk.inventoryservice.dto.messages.OrderItemDebeziumMessage;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String, String> OrderConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "inventory-service");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, OrderDebeziumMessage.class.getName());
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean(name = "OrderKafkaListenerContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory<String, String> OrderKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(OrderConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> OrderItemConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "inventory-service");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, OrderItemDebeziumMessage.class.getName());
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean(name = "OrderItemKafkaListenerContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory<String, String> OrderKafkaListenerContainerFactory2() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(OrderItemConsumerFactory());
+        return factory;
+    }
 }
